35c7972f37b30c03c875e14d90813472cd6af54c
yoyo9747 <josephro9747@gmail.com>
2023-11-15 09:38:40 +0000

    before_compile

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT_DYNAMIC */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#1 SMP PREEMPT_DYNAMIC Wed Nov 15 09:29:30 UTC 2023"
+#define LINUX_COMPILE_BY "femu"
+#define LINUX_COMPILE_HOST "fvm"
+#define LINUX_COMPILER "gcc (Ubuntu 9.4.0-1ubuntu1~20.04.2) 9.4.0, GNU ld (GNU Binutils for Ubuntu) 2.34"
